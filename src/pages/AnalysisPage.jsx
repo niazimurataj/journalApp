f2252a8d2ca92ca48f2c18df879e1dd6484f25fb
Zeke Murataj <niazi@murataj.com>
2025-07-10 23:36:11 -0700

    Add planning page scaffold

--- src/pages/AnalysisPage.jsx
+++ src/pages/AnalysisPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AnalysisSidebar from '../components/AnalysisSidebar';
 
+
 function AnalysisPage() {
     return (
         <div>
             <h1>Analysis Page</h1>
+            {/* TODO: implement sentiment and tone analysis with draggable cards*/}
 
         </div>
     );
---
+++ src/pages/PlanPage.jsx
@@ -0,0 +1,14 @@
+// Todo: implement frontlog, backlog, tracking, and done pages here
+// Todo: implement done page analysis 
+
+import React from 'react';
+
+function PlanPage() {
+    return (
+        <div>
+            <h1>Plan Page</h1>
+        </div>
+    );
+}
+
+export default PlanPage;
