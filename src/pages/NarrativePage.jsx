64f15ed9d2f7545b2db9cc34d65f2218e84e0cc2
Zeke Murataj <niazi@murataj.com>
2025-08-11 00:16:17 -0700

    Define narrative page in more depth

---
+++ src/pages/NarrativePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './NarrativePage.module.css'; // Assuming you have a CSS module for styles
+
+function NarrativePage() {
+
+}
+
+export default NarrativePage; 
